a3b2df01d1f74319b753e9306ce09e08c622a342
tikamkasper <tikamkasper@gmail.com>
2025-02-03 18:30:58 +0530

    add new

--- src/components/header/HeaderMiddle.jsx
+++ src/components/header/HeaderMiddle.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router";
 import { CiSearch } from "react-icons/ci";
 import { FaRegUserCircle } from "react-icons/fa";
 import { BsCart4 } from "react-icons/bs";
-import { GiDivingDagger, GiHamburgerMenu } from "react-icons/gi";
-import logo from "../../assets/kasper_logo.png";
+import logo from "../../assets/ks_logo.png";
 
 const HeaderMiddle = () => {
+  const [isShowUserDropDown, setIsShowUserDropDown] = useState(false);
+
+  const menuRef = useRef();
+  const userIconRef = useRef();
+
+  // window.addEventListener("click", (e) => {
+  //   if (e.target !== menuRef.current) {
+  //     setIsShowUserDropDown(false);
+  //   }
+  // });
+
   return (
     <>
       <div className="header-middle">
@@ -29,10 +39,50 @@ const HeaderMiddle = () => {
             </form>
           </div>
           <div className="icons">
-            <div className="user">
-              <button>
+            <div className="user" ref={userIconRef}>
+              <button
+                type="button"
+                onClick={() => setIsShowUserDropDown(!isShowUserDropDown)}
+              >
                 <FaRegUserCircle />
               </button>
+              {isShowUserDropDown && (
+                <div className="userDropDown" ref={menuRef}>
+                  <div className="user-greet">
+                    <p>Hello {"User"} !</p>
+                    <p>Welcome to {"K_SHOP"} !!</p>
+                  </div>
+                  <ul>
+                    <li>
+                      <Link onClick={() => setIsShowUserDropDown(false)}>
+                        Login/Signup
+                      </Link>
+                    </li>
+                    <li>
+                      <Link onClick={() => setIsShowUserDropDown(false)}>
+                        {" "}
+                        Logout
+                      </Link>
+                    </li>
+                    <li>
+                      <Link onClick={() => setIsShowUserDropDown(false)}>
+                        Profile
+                      </Link>
+                    </li>
+                    <li>
+                      <Link onClick={() => setIsShowUserDropDown(false)}>
+                        Dashboard
+                      </Link>
+                    </li>
+                    <li>
+                      <Link onClick={() => setIsShowUserDropDown(false)}>
+                        {" "}
+                        My Orders
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              )}
             </div>
             <div className="cart">
               <div className="cart-icon">
@@ -43,11 +93,6 @@ const HeaderMiddle = () => {
               <div className="cart-count">5</div>
             </div>
           </div>
-          {/* <div className="hamburger-menu">
-            <button>
-              <GiHamburgerMenu />
-            </button>
-          </div> */}
         </div>
       </div>
     </>
